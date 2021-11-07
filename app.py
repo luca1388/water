5a4f7c9fa0ec2e59bdcf6a2e6d5e5e19ed7e517b
Luca Gandini <lgandini@funambol.com>
2021-11-07 19:11:57 +0100

    body structure

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import RPi.GPIO as GPIO
+from time import sleep
 import json
 from flask import Flask, request, jsonify
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)   # Set pin 11 to be an output pin and set initial value to low (off)
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)   # Set pin 13 to be an output pin and set initial value to low (off)
+GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)   # Set pin 15 to be an output pin and set initial value to low (off)
 
 app = Flask(__name__)
 
@@ -13,8 +22,11 @@ def get_relay(relay_id):
 
 @app.route('/relays/<relay_id>', methods=['PUT'])
 def update_relay(relay_id):
-    data = json.loads(request.data)
-    return ''+ data
+    body = json.loads(request.data)
+    print body
+    bodyState = body['data']['state']
+
+    return ''+ bodyState
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
