5ac7edc2250366548631daf33754e862b8a43467
Luca Gandini <lgandini@funambol.com>
2021-11-07 20:46:07 +0100

    js file

--- static/main.js
+++ static/main.js
@@ -1,12 +1,21 @@
-document.addEventListener(
-  document.getElementById("checkbox-zone-a"),
-  "click",
-  function () {
-      var checkboxA = document.getElementById("checkbox-zone-a");
-      if (checkboxA.checked) {
-        console.log('A: checked');
-      } else {
-        console.log('A: NOT checked');
-      }
-  }
-);
+document
+  .getElementById("checkbox-zone-a")
+  .addEventListener("click", function () {
+    var checkboxA = document.getElementById("checkbox-zone-a");
+    if (checkboxA.checked) {
+      console.log("A: checked");
+    } else {
+      console.log("A: NOT checked");
+    }
+  });
+
+  document
+  .getElementById("checkbox-zone-b")
+  .addEventListener("click", function () {
+    var checkboxB = document.getElementById("checkbox-zone-b");
+    if (checkboxB.checked) {
+      console.log("B: checked");
+    } else {
+      console.log("B: NOT checked");
+    }
+  });
