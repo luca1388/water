17beb9a5223356a7b9d68faeeac57f7955ffc84a
Luca Gandini <lgandini@funambol.com>
2021-11-07 19:36:24 +0100

    return message

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ from flask import Flask, request, jsonify
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 
-GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)   # Set pin 11 to be an output pin and set initial value to low (off)
-GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)   # Set pin 13 to be an output pin and set initial value to low (off)
-GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)   # Set pin 15 to be an output pin and set initial value to low (off)
+VALVE_A_OPENED_PIN = 11
+VALVE_A_CLOSED_PIN = 13
+VALVE_B_OPENED_PIN = 15
+VALVE_B_CLOSED_PIN = 16
+
+GPIO.setup(VALVE_A_OPENED_PIN, GPIO.OUT, initial=GPIO.LOW)   # Set pin 11 to be an output pin and set initial value to low (off)
+GPIO.setup(VALVE_A_CLOSED_PIN, GPIO.OUT, initial=GPIO.LOW)   # Set pin 13 to be an output pin and set initial value to low (off)
+GPIO.setup(VALVE_B_OPENED_PIN, GPIO.OUT, initial=GPIO.LOW)   # Set pin 15 to be an output pin and set initial value to low (off)
+GPIO.setup(VALVE_B_CLOSED_PIN, GPIO.OUT, initial=GPIO.LOW)   # Set pin 15 to be an output pin and set initial value to low (off)
 
 app = Flask(__name__)
 
@@ -16,20 +22,26 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
-@app.route('/relays/<relay_id>', methods=['GET'])
-def get_relay(relay_id):
-    return ''+ relay_id
+@app.route('/valves/<valve_id>', methods=['GET'])
+def get_relay(valve_id):
+    return ''+ valve_id
 
-@app.route('/relays/<relay_id>', methods=['PUT'])
-def update_relay(relay_id):
+@app.route('/valves/<valve_id>', methods=['PUT'])
+def update_relay(valve_id):
     try:
         body = json.loads(request.data)
         print body
-        bodyState = body['data']['state']
+        body_state = body['data']['state']
     except:
         return 'malformed request'
 
-    return ''+ bodyState
+    if body_state == "on":
+        status_message = 'opening valve' + valve_id
+    elif body_state == "off":
+        status_message = 'closing valve' + valve_id
+
+    print (status_message)
+    return status_message
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
