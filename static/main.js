290469ab9abae07b498522e72a573d33ca565937
unknown <luca.gandini88@gmail.com>
2024-06-22 19:25:54 +0200

    feat: 4 zones

--- static/main.js
+++ static/main.js
@@ -40,3 +40,45 @@ document
         .then((result) => console.log(result))
         .catch((e) => console.log(e));
   });
+
+  document
+  .getElementById("checkbox-zone-c")
+  .addEventListener("click", function () {
+    var checkboxB = document.getElementById("checkbox-zone-c");
+    var state = 'off';
+    if (checkboxB.checked) {
+      console.log("C: checked");
+      state = 'on';
+    } else {
+      console.log("C: NOT checked");
+      state = 'off';
+    }
+    fetch("/valves/c", {
+        method: "PUT",
+        body: JSON.stringify({ data: { state: state } }),
+      })
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+        .catch((e) => console.log(e));
+  });
+
+  document
+  .getElementById("checkbox-zone-d")
+  .addEventListener("click", function () {
+    var checkboxB = document.getElementById("checkbox-zone-d");
+    var state = 'off';
+    if (checkboxB.checked) {
+      console.log("D: checked");
+      state = 'on';
+    } else {
+      console.log("D: NOT checked");
+      state = 'off';
+    }
+    fetch("/valves/d", {
+        method: "PUT",
+        body: JSON.stringify({ data: { state: state } }),
+      })
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+        .catch((e) => console.log(e));
+  });
